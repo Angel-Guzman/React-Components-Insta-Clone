97c96cfae3de68d5cb177140d18b2b08b6ce11a6
Angelg10 <angelguzman97@gmail.com>
2020-04-14 18:26:01 -0400

    finished MVP

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+// import useState
+import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 import PostsPage from "./components/PostsContainer/PostsPage"
 import SearchBar from "./components/SearchBar/SearchBarContainer"
+// import data 
+import dummyData from "./dummy-data"
 
 const App = () => {
+  // set up state for your data
+  const [posts, setPosts] = useState(dummyData);
 
   return (
     <div className="App">
       {/* Add imported components here to render them */}
-      <PostsPage /> 
       <SearchBar /> 
+      <PostsPage posts={posts} /> 
     </div>
   );
 };
